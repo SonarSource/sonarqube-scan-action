0feedead34b44be02821cf518b0442e3d5465f0a
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-11-26 09:57:41 +0100

    Code review: remove unnecessary client side certificate generation

--- .github/qa-sq-behind-ngix/generate-certificates.sh
+++ .github/qa-sq-behind-ngix/generate-certificates.sh
@@ -15,7 +15,7 @@ openssl req \
     -subj "/C=CH/ST=Geneva/L=Geneva/O=Server/OU=Dept" \
     -keyout server.key
 
-echo Generating client certificate...
+echo Generating CA certificate...
 
 # Generate Certificate Authority key
 openssl genrsa \
@@ -32,38 +32,3 @@ openssl req \
     -key ca.key \
     -out ca.crt \
     -subj "/C=CH/ST=Geneva/L=Geneva/O=CertificateAuthority/OU=ExpertDepartment"
-
-# Generating Client certificate key
-openssl genrsa \
-    -passout pass:test42 \
-    -des3 \
-    -out user.key 4096
-
-# Generating Client certificate certificate
-openssl req \
-    -passin pass:test42 \
-    -new \
-    -key user.key \
-    -out user.csr \
-    -subj "/C=CH/ST=Geneva/L=Geneva/O=UserOrg/OU=UserDepartment"
-
-# Sign the certificate
-openssl x509 \
-    -passin pass:test42 \
-    -req \
-    -days 365 \
-    -in user.csr \
-    -CA ca.crt \
-    -CAkey ca.key \
-    -set_serial 01 \
-    -out user.crt
-
-# Generate a PKCS12 format certificate
-openssl pkcs12 \
-    -passin pass:test42 \
-    -passout pass:test42 \
-    -export \
-    -out user.p12 \
-    -inkey user.key \
-    -in user.crt \
-    -certfile ca.crt
