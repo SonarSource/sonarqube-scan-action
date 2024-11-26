4c0dc43ef8ad14206f9f84a54be39420995a8712
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-11-26 19:19:07 +0100

    SQSCANGHA-55 Support GitHub self-hosted runners without wget

--- install-sonar-scanner-cli.sh
+++ install-sonar-scanner-cli.sh
@@ -2,16 +2,20 @@
 
 set -eou pipefail
 
-#See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+# See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+#
+# Script-specific variables required:
+# - INPUT_SCANNERVERSION: e.g. 6.2.1.4610
+# - INPUT_SCANNERBINARIESURL: e.g. https://github.com/me/my-repo/raw/refs/heads/main/binaries
 
-WGET=wget
+CURL=curl
 if [[ "$RUNNER_OS" == "Linux" &&  "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="linux-x64"
 elif [[ "$RUNNER_OS" == "Linux" &&  "$RUNNER_ARCH" == "ARM64" ]]; then
      FLAVOR="linux-aarch64"
 elif [[ "$RUNNER_OS" == "Windows" &&  "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="windows-x64"
-     WGET="C:\\msys64\\usr\\bin\\wget.exe"
+     CURL="C:\\msys64\\usr\\bin\\curl.exe"
 elif [[ "$RUNNER_OS" == "macOS" &&  "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="macosx-x64"
 elif [[ "$RUNNER_OS" == "macOS" &&  "$RUNNER_ARCH" == "ARM64" ]]; then
@@ -26,9 +30,11 @@ set -x
 mkdir -p $RUNNER_TEMP/sonarscanner
 cd $RUNNER_TEMP/sonarscanner
 
-$WGET --no-verbose --user-agent="sonarqube-scan-action" "${INPUT_SCANNERBINARIESURL%/}/sonar-scanner-cli-$INPUT_SCANNERVERSION-$FLAVOR.zip"
+SCANNER_FILE_NAME="sonar-scanner-cli-$INPUT_SCANNERVERSION-$FLAVOR.zip"
+SCANNER_URI="${INPUT_SCANNERBINARIESURL%/}/$SCANNER_FILE_NAME"
+$CURL --user-agent "sonarqube-scan-action" --output $SCANNER_FILE_NAME $SCANNER_URI
 
-unzip -q sonar-scanner-cli-$INPUT_SCANNERVERSION-$FLAVOR.zip
+unzip -q $SCANNER_FILE_NAME
 
 # Folder name should correspond to the directory cached by the actions/cache
 mv sonar-scanner-$INPUT_SCANNERVERSION-$FLAVOR $RUNNER_TEMP/sonar-scanner-cli-$INPUT_SCANNERVERSION-$RUNNER_OS-$RUNNER_ARCH
