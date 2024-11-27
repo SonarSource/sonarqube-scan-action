a9c2193f873a356daa25d9e2377ef9ab680e4b2b
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-11-27 09:12:49 +0100

    Add fallback mechanism

--- install-sonar-scanner-cli.sh
+++ install-sonar-scanner-cli.sh
@@ -8,21 +8,39 @@ set -eou pipefail
 # - INPUT_SCANNERVERSION: e.g. 6.2.1.4610
 # - INPUT_SCANNERBINARIESURL: e.g. https://github.com/me/my-repo/raw/refs/heads/main/binaries
 
-CURL=curl
-if [[ "$RUNNER_OS" == "Linux" &&  "$RUNNER_ARCH" == "X64" ]]; then
+if [[ "$RUNNER_OS" == "Linux" && "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="linux-x64"
-elif [[ "$RUNNER_OS" == "Linux" &&  "$RUNNER_ARCH" == "ARM64" ]]; then
+elif [[ "$RUNNER_OS" == "Linux" && "$RUNNER_ARCH" == "ARM64" ]]; then
      FLAVOR="linux-aarch64"
-elif [[ "$RUNNER_OS" == "Windows" &&  "$RUNNER_ARCH" == "X64" ]]; then
+elif [[ "$RUNNER_OS" == "Windows" && "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="windows-x64"
-     CURL="C:\\msys64\\usr\\bin\\curl.exe"
-elif [[ "$RUNNER_OS" == "macOS" &&  "$RUNNER_ARCH" == "X64" ]]; then
+elif [[ "$RUNNER_OS" == "macOS" && "$RUNNER_ARCH" == "X64" ]]; then
      FLAVOR="macosx-x64"
-elif [[ "$RUNNER_OS" == "macOS" &&  "$RUNNER_ARCH" == "ARM64" ]]; then
+elif [[ "$RUNNER_OS" == "macOS" && "$RUNNER_ARCH" == "ARM64" ]]; then
      FLAVOR="macosx-aarch64"
 else
-     echo "$RUNNER_OS $RUNNER_ARCH not supported"
+     echo "::error title=SonarScanner::$RUNNER_OS $RUNNER_ARCH not supported"
      exit 1
+fi
+
+SCANNER_FILE_NAME="sonar-scanner-cli-$INPUT_SCANNERVERSION-$FLAVOR.zip"
+SCANNER_URI="${INPUT_SCANNERBINARIESURL%/}/$SCANNER_FILE_NAME"
+
+if command -v wget &> /dev/null; then
+    DOWNLOAD_COMMAND="wget"
+    DOWNLOAD_ARGS="--no-verbose --user-agent=sonarqube-scan-action $SCANNER_URI"
+elif command -v curl &> /dev/null; then
+    DOWNLOAD_COMMAND="curl"
+    DOWNLOAD_ARGS="--silent --show-error --user-agent sonarqube-scan-action --output $SCANNER_FILE_NAME $SCANNER_URI"
+elif [ "$RUNNER_OS" == "Windows" ] && [ -t "C:\\msys64\\usr\\bin\\wget.exe" ]; then
+    DOWNLOAD_COMMAND="C:\\msys64\\usr\\bin\\wget.exe"
+    DOWNLOAD_ARGS="--no-verbose --user-agent=sonarqube-scan-action $SCANNER_URI"
+elif [ "$RUNNER_OS" == "Windows" ] && [ -t "C:\\msys64\\usr\\bin\\curl.exe" ]; then
+    DOWNLOAD_COMMAND="C:\\msys64\\usr\\bin\\curl.exe"
+    DOWNLOAD_ARGS="--silent --show-error --user-agent sonarqube-scan-action --output $SCANNER_FILE_NAME $SCANNER_URI"
+else
+    echo "::error title=SonarScanner::Neither wget nor curl found on the machine"
+    exit 1
 fi
 
 set -x
@@ -30,9 +48,7 @@ set -x
 mkdir -p $RUNNER_TEMP/sonarscanner
 cd $RUNNER_TEMP/sonarscanner
 
-SCANNER_FILE_NAME="sonar-scanner-cli-$INPUT_SCANNERVERSION-$FLAVOR.zip"
-SCANNER_URI="${INPUT_SCANNERBINARIESURL%/}/$SCANNER_FILE_NAME"
-$CURL --user-agent "sonarqube-scan-action" --output $SCANNER_FILE_NAME $SCANNER_URI
+$DOWNLOAD_COMMAND $DOWNLOAD_ARGS
 
 unzip -q $SCANNER_FILE_NAME
 
