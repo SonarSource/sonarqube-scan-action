00e62e119083df8c4315d967fb0fcfb4a1895444
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-12-16 10:24:14 +0100

    SQCPPGHA-9 Extend action to support C, C++, and Objective-C projects (#161)

---
+++ scripts/cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -n "${SONAR_ROOT_CERT}" ]]; then
+  echo "Adding custom root certificate to java certificate store"
+  rm -f /tmp/tmpcert.pem
+  echo "${SONAR_ROOT_CERT}" > /tmp/tmpcert.pem
+  keytool -keystore /etc/ssl/certs/java/cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias sonarqube -file /tmp/tmpcert.pem
+fi
