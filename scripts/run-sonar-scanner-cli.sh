4d448d096325f5f62b49c13d1139f70ee2fe94ab
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-12-16 09:42:42 +0100

    SQSCANGHA-76 Support self-hosted runners not clearing truststore after run

--- scripts/run-sonar-scanner-cli.sh
+++ scripts/run-sonar-scanner-cli.sh
@@ -21,6 +21,13 @@ if [[ -n "${INPUT_PROJECTBASEDIR}" ]]; then
   scanner_args+=("-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR}")
 fi
 
+# The SSL folder may exist on an uncleaned self-hosted runner
+SONAR_SSL_FOLDER=~/.sonar/ssl
+if [ -d "$SONAR_SSL_FOLDER" ]; then
+  echo "::warning title=SonarScanner::Cleaning existing SSL folder: $SONAR_SSL_FOLDER"
+  rm -rf "$SONAR_SSL_FOLDER"
+fi
+
 if [[ -n "${SONAR_ROOT_CERT}" ]]; then
   echo "Adding SSL certificate to the Scanner truststore"
   rm -f $RUNNER_TEMP/tmpcert.pem
@@ -28,8 +35,8 @@ if [[ -n "${SONAR_ROOT_CERT}" ]]; then
   # Use keytool for now, as SonarQube 10.6 and below doesn't support openssl generated keystores
   # keytool require a password > 6 characters, so we wan't use the default password 'sonar'
   store_pass=changeit
-  mkdir -p ~/.sonar/ssl
-  $SONAR_SCANNER_JRE/bin/java sun.security.tools.keytool.Main -storetype PKCS12 -keystore ~/.sonar/ssl/truststore.p12 -storepass $store_pass -noprompt -trustcacerts -importcert -alias sonar -file $RUNNER_TEMP/tmpcert.pem
+  mkdir -p "$SONAR_SSL_FOLDER"
+  $SONAR_SCANNER_JRE/bin/java sun.security.tools.keytool.Main -storetype PKCS12 -keystore $SONAR_SSL_FOLDER/truststore.p12 -storepass $store_pass -noprompt -trustcacerts -importcert -alias sonar -file $RUNNER_TEMP/tmpcert.pem
   scanner_args+=("-Dsonar.scanner.truststorePassword=$store_pass")
 fi
 
